d266b4ed3316a63599feec2ed26657f27f4f0374
Andy <66146370+andosgee@users.noreply.github.com>
2025-01-30 14:40:22 +1300

    Implement Popup for Location
    Next is to stlye the PAC container, make the span change and get the city
    
    restrict the searches to NZ only

--- AraEats Frontend/src/components/home/HomeHeader.tsx
+++ AraEats Frontend/src/components/home/HomeHeader.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CuisineTypeCarousel from './CuisineTypeCarousel';
+import AutoCompAddress from './AutoCompAddress';
 import LocationSVG from '../../assets/svg/location-pin-svg';
-import '../../styles/home/homeHeader.css'
+import '../../styles/home/homeHeader.css';
+
 
 export default function HomeHeader() {
-    let changeLocationPopup = () => {
-        console.log('Change button clicked.')
-    }
+    const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+    const changeLocationPopup = () => {
+        setIsPopupVisible(true);
+    };
+
+    const closePopup = () => {
+        setIsPopupVisible(false);
+    };
+
+    const savePopup = () => {
+        setIsPopupVisible(false);
+    };
 
     return (
         <>
@@ -14,10 +26,19 @@ export default function HomeHeader() {
             <span>
                 <LocationSVG />
                 <p>Christchruch, New Zealand</p>
+                {/* Location change to the City and Country of prompt */}
                 <a onClick={() => { changeLocationPopup() }}><strong>Change...</strong></a>
             </span>
+            {/* Popup Window */}
+            {isPopupVisible && (
+                    <div className="popUpWindow">
+                        <button className='close' onClick={closePopup}>Close</button>
+                        <button className='save' onClick={savePopup}>Save Location</button>
+                        <AutoCompAddress />
+                    </div>
+                )}
             <form>
-                <input type="text" placeholder="Start typing..."/>
+                <input type="text" placeholder='Search Here...' />
             </form>
         </div>
         <CuisineTypeCarousel />
